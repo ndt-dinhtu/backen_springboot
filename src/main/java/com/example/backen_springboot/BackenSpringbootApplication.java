a2b399ea96c7d2f9aff8e1b63c66cf300f8c8967
ndt-dinhtu <ndt.290303060703@gmail.com>
2024-09-16 21:15:12 +0700

    frist commmit

---
+++ src/main/java/com/example/backen_springboot/BackenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backen_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackenSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backen_springboot/BackenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backen_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackenSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
