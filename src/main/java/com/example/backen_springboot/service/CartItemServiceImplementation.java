e4ce5a93cf5989365d9aee2e09ce627ee8199da5
ndt-dinhtu <ndt.290303060703@gmail.com>
2024-09-19 23:52:42 +0700

    ccartItemServiceImplementation

--- src/main/java/com/example/backen_springboot/service/CartItemServiceImplementation.java
+++ src/main/java/com/example/backen_springboot/service/CartItemServiceImplementation.java
@@ -5,10 +5,13 @@ import com.example.backen_springboot.exception.UserException;
 import com.example.backen_springboot.model.Cart;
 import com.example.backen_springboot.model.CartItem;
 import com.example.backen_springboot.model.Product;
+import com.example.backen_springboot.model.User;
 import com.example.backen_springboot.repository.CartItemRepository;
 import com.example.backen_springboot.repository.CartRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,26 +23,49 @@ public class CartItemServiceImplementation implements CartItemService {
 
     @Override
     public CartItem createCartItem(CartItem cartItem) {
-        return null;
+        cartItem.setQuantity(1);
+        cartItem.setPrice(cartItem.getProduct().getPrice() * cartItem.getQuantity());
+        cartItem.setDiscountedPrice(cartItem.getProduct().getDiscountedPrice() * cartItem.getQuantity());
+        CartItem createdCartItem = cartItemRepository.save(cartItem);
+        return createdCartItem;
     }
 
     @Override
     public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) throws CartItemException, UserException {
-        return null;
+        CartItem item = findCartItemById(id);
+        User user = userService.findUserById(item.getUserId());
+        if (user.getId().equals(userId)) {
+            item.setQuantity(cartItem.getQuantity());
+            item.setPrice(item.getQuantity() * item.getProduct().getPrice());
+            item.setDiscountedPrice(item.getProduct().getDiscountedPrice() * item.getQuantity());
+        }
+        return cartItemRepository.save(item);
     }
 
     @Override
     public CartItem isCartItemExist(Cart cart, Product product, String size, Long userId) {
-        return null;
+        CartItem cartItem = cartItemRepository.isCartItemExist(cart, product, size, userId);
+        return cartItem;
     }
 
     @Override
     public void removeCartItem(Long userId, Long cartItemId) throws CartItemException, UserException {
-
+        CartItem cartItem = findCartItemById(cartItemId);
+        User user = userService.findUserById(cartItem.getUserId());
+        User reqUser = userService.findUserById(userId);
+        if (user.getId().equals(reqUser.getId())) {
+            cartItemRepository.deleteById(cartItemId);
+        } else {
+            throw new UserException("you can't remove another users item");
+        }
     }
 
     @Override
     public CartItem findCartItemById(Long cartItemId) throws CartItemException {
-        return null;
+        Optional<CartItem> opt =cartItemRepository.findById(cartItemId);
+        if(opt.isPresent()) {
+            return opt.get();
+        }
+        throw new CartItemException("cartItem not found with id : "+cartItemId);
     }
 }
