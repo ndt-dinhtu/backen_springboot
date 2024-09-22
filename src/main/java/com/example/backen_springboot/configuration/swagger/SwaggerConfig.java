c4406bb175fee9a2b817902734b23997044c6874
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-09-22 08:26:57 +0700

    [21/9/2024]- fix cors and bear token in documentary swagger

--- src/main/java/com/example/backen_springboot/configuration/swagger/SwaggerConfig.java
+++ src/main/java/com/example/backen_springboot/configuration/swagger/SwaggerConfig.java
@@ -3,12 +3,15 @@ package com.example.backen_springboot.configuration.swagger;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 import java.util.List;
 
@@ -30,7 +33,14 @@ public class SwaggerConfig {
                         .description("This is a sample API")
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0.html"))
                 )
-                .servers(List.of(new Server().url("http://localhost:8088").description("this is the localhost server")));
+                .servers(List.of(new Server().url("http://localhost:8080").description("this is the localhost server")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 
     @Bean
