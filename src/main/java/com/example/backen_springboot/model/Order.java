7d64f9c2a6cb30b5adacf7dad1ec8050a899a294
ndt-dinhtu <ndt.290303060703@gmail.com>
2024-09-24 10:21:50 +0700

    fix order

--- src/main/java/com/example/backen_springboot/model/Order.java
+++ src/main/java/com/example/backen_springboot/model/Order.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "`order`")`
+@Table(name = "`orders`")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
